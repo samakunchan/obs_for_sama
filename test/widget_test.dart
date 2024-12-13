a761672117b79345b4e662bd5b13c5d35dcd52fe
samakunchan <cedric.badjah@gmail.com>
2024-12-13 11:49:51 +0100

    feat: structures
    - Updates structures of main app.
    - Update path for integration test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:obs_for_sama/main.dart';
+import 'package:obs_for_sama/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
