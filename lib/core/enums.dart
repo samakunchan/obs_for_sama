233b42f245cea24a091ca9c78b0920d0db7cde3e
samakunchan <cedric.badjah@gmail.com>
2024-12-13 11:48:50 +0100

    feat: enums
    - Added a new enums to manage static text in the app.

--- lib/core/enums.dart
+++ lib/core/enums.dart
@@ -7,3 +7,19 @@ enum SettingsEnum {
 
   final String label;
 }
+
+enum AppText {
+  mainTitle(label: 'OBS MANAGER'),
+  title(label: 'PLAY WITH OBS'),
+  obsServerForm(label: 'SERVER PARAMETERS'),
+  connectToOBS(label: 'CONNECT OBS'),
+  start(label: 'START STREAM'),
+  stop(label: 'STOP STREAM'),
+  scenes(label: 'SCENES'),
+  sources(label: 'SOURCES'),
+  ;
+
+  const AppText({required this.label});
+
+  final String label;
+}
