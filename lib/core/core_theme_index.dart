e7b8132858a54893f83ff6dc931812d83a1ef3e9
samakunchan <cedric.badjah@gmail.com>
2025-08-28 10:02:45 +0200

    feat: path
    - Added a core file for the path of each librairy.

---
+++ lib/core/core_theme_index.dart
@@ -0,0 +1,39 @@
+/// CORE THEME
+library;
+
+/// ENUMS
+export 'enums.dart';
+
+/// PAINTERS
+export 'painters.dart';
+
+/// ROUTES
+export 'routes.dart';
+
+/// SERVER BINDING GETX
+export 'server_binding.dart';
+
+/// THEMES
+/// THEMES - APP BAR
+export 'themes/app_bar_theme.dart';
+
+/// THEMES - BUTTONS
+export 'themes/button_theme.dart';
+
+/// THEMES - CARD
+export 'themes/card_theme.dart';
+
+/// THEMES - Colors
+export 'themes/color_scheme.dart';
+
+/// THEMES - DIALOG
+export 'themes/dialog_theme.dart';
+
+/// THEMES - INPUT
+export 'themes/input_decoration_theme.dart';
+
+/// THEMES - TABS BAR
+export 'themes/tab_bar_theme.dart';
+
+/// THEMES - TEXT
+export 'themes/text_theme.dart';
