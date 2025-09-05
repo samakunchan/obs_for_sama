5c713c8d98dd3fec83b24824dfd872be3001f63b
samakunchan <cedric.badjah@gmail.com>
2025-09-05 16:30:22 +0200

    feat: BIG SEPERATION

---
+++ lib/app_with_get_x/core/controllers/lifecyle/life_cycle_controller.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:obs_for_sama/app_with_get_x/core/controllers/obs/server_controller.dart';
+
+class LifeCycleController extends FullLifeCycleController with FullLifeCycleMixin {
+  final ServerController controller = Get.find();
+
+  @override
+  void onDetached() {
+    controller.logoutToOBS();
+  }
+
+  @override
+  void onHidden() {
+    controller.logoutToOBS();
+  }
+
+  @override
+  void onInactive() {
+    controller.logoutToOBS();
+  }
+
+  @override
+  void onPaused() {
+    controller.logoutToOBS();
+  }
+
+  @override
+  void onResumed() {
+    controller.connectToOBS();
+  }
+}
--- lib/core/themes/card_theme.dart
+++ lib/core/themes/card_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:obs_for_sama/core/core_theme_index.dart';
+import 'package:obs_for_sama/core/index.dart';
 
 CardThemeData kCardTheme = CardThemeData(
   color: kPrimaryColor.withValues(alpha: .5),
