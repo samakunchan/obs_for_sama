2b80ca8ba0870b1db92f929796299be66985a17f
samakunchan <cedric.badjah@gmail.com>
2025-09-08 16:44:47 +0200

    feat: drawer
    - Added custom widget for the drawer.

---
+++ lib/app_with_flutter_bloc/presentions/widgets/o_b_s_drawer.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:obs_for_sama/core/paints/clippers.dart';
+import 'package:obs_for_sama/core/themes/color_scheme.dart';
+import 'package:obs_for_sama/core/themes/text_theme.dart';
+import 'package:obs_for_sama/core/widgets/r_s_i_outlined_body.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class OBSDrawer extends StatelessWidget {
+  const OBSDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RSIOutlinedBody(
+      height: double.infinity,
+      width: MediaQuery.of(context).size.width * .8,
+      edgeClipper: const RSIEdgeClipper(
+        edgeRightTop: true,
+        edgeRightBottom: true,
+      ),
+      child: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: DrawerHeader(
+                  child: Image.asset('assets/icon-ios-obs-manager-x1.png'),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('MENU_INFOS_VERSION', style: ktitle2),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      title: Text('MENU_WORK_FLOW', style: ktitle2),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      title: Text('MENU_CONF_TWITCH', style: ktitle2),
+                    ),
+                    ListTile(
+                      title: Text('MENU_THEME_SELECT', style: ktitle2),
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      iconColor: kTextShadow,
+                      title: Text('MENU_SETTINGS', style: ktitle2),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/settings');
+                      },
+                    ),
+                    const Divider(),
+                  ],
+                ),
+              ),
+              FutureBuilder(
+                future: PackageInfo.fromPlatform(),
+                builder: (BuildContext context, AsyncSnapshot<PackageInfo> snapshot) {
+                  if (kDebugMode) {
+                    print(snapshot.data?.version);
+                    print('Si la version n‘est pas la même. Il faut cut/restart.');
+                  }
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsetsGeometry.symmetric(vertical: 16),
+                      child: Text(
+                        'OBSM v${snapshot.data?.version}',
+                        style: kbodyLarge.copyWith(color: kTextShadow),
+                      ),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return Text(
+                      '${snapshot.error}',
+                      style: kbodyLarge,
+                    );
+                  }
+                  return const SizedBox();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
